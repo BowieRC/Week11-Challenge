3f2cd14c0eeb9ea50e5ac1e3cd69bba7d3f0e1d8
BowieRC <rowlandscrispebowie@gmail.com>
2023-11-27 15:47:04 +0930

    Heroku port modification

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
